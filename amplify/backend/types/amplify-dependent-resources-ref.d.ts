f10f5d426dc516bf11de2bee1794cd85c7235cf6
Nicholas VanDyken <vandyken.nicholas@gmail.com>
2022-10-25 15:25:36 -0400

    added gql and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "amplifityreactv1": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "s35502b665": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
